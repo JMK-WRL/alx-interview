e887e66fcdceef1510589d65f5acd323c017d48b
Jonathan Kyule <jonathankyule2@gmail.com>
2024-04-25 07:02:45 +0000

    task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+def minOperations(n):
+    if n <= 1:
+        return n
+
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+
+    for i in range(2, n + 1 ):
+        for j in range(1, i // 2 + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n]
