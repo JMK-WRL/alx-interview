cd38671c8dd396ea7eff4a8d493d23bf9dcaf7d5
Jonathan Kyule <jonathankyule2@gmail.com>
2024-05-02 20:01:08 +0000

    changes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,51 @@
 #!/usr/bin/python3
 
-"""Script that reads stdin line by line and computes metrics"""
-
 import sys
 
+def print_stats(status_codes, total_file_size):
+    """
+    Print statistics
+    Args:
+        status_codes: dictionary of status codes
+        total_file_size: total size of files
+    Returns:
+        None
+    """
+    print("Total File Size: {}".format(total_file_size))
+    for code, count in sorted(status_codes.items()):
+        if count != 0:
+            print("{}: {}".format(code, count))
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
-
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+total_file_size = 0
+counter = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+        parts = line.split()[::-1]  # Splitting and reversing the line
 
-        stlist = line.split()
-        count += 1
+        if len(parts) > 2:
+            counter += 1
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
+            if counter <= 10:
+                total_file_size += int(parts[0])  # File size
+                code = parts[1]  # Status code
 
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
+                if code in status_codes:
+                    status_codes[code] += 1
 
+            if counter == 10:
+                print_stats(status_codes, total_file_size)
+                counter = 0
 
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+finally:
+    print_stats(status_codes, total_file_size)
