2d453532ab1dbe5a73578b92cac304d78c18410f
Jonathan Kyule <jonathankyule2@gmail.com>
2024-05-02 20:03:04 +0000

    updates

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,50 +2,53 @@
 
 import sys
 
-def print_stats(status_codes, total_file_size):
+
+def print_msg(dict_sc, total_file_size):
     """
-    Print statistics
+    Method to print
     Args:
-        status_codes: dictionary of status codes
-        total_file_size: total size of files
+        dict_sc: dict of status codes
+        total_file_size: total of the file
     Returns:
-        None
+        Nothing
     """
-    print("Total File Size: {}".format(total_file_size))
-    for code, count in sorted(status_codes.items()):
-        if count != 0:
-            print("{}: {}".format(code, count))
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
 
 total_file_size = 0
+code = 0
 counter = 0
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        parts = line.split()[::-1]  # Splitting and reversing the line
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-        if len(parts) > 2:
+        if len(parsed_line) > 2:
             counter += 1
 
             if counter <= 10:
-                total_file_size += int(parts[0])  # File size
-                code = parts[1]  # Status code
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-                if code in status_codes:
-                    status_codes[code] += 1
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-            if counter == 10:
-                print_stats(status_codes, total_file_size)
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
                 counter = 0
 
 finally:
-    print_stats(status_codes, total_file_size)
+    print_msg(dict_sc, total_file_size)
