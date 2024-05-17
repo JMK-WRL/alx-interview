efcdd2a0c7bf95bccd45b7b038c30a62efba9c22
Jonathan Kyule <jonathankyule2@gmail.com>
2024-05-17 02:05:42 +0000

    updates

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,135 @@
 #!/usr/bin/python3
-""" N queens """
+"""
+Solves the N-queens problem.
+Finds all possible arrangements of N
+non-conflicting queens on an NxN chessboard.
+Example:
+    $ ./0-nqueens.py N
+N must be an integer greater than or equal to 4.
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+Solutions are represented as [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen is placed on the chessboard.
+"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
+def create_board(n):
+    """Create an `n`x`n` chessboard initialized with spaces."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return board
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+def deep_copy_board(board):
+    """Return a deep copy of the chessboard."""
+    if isinstance(board, list):
+        return list(map(deep_copy_board, board))
+    return board
 
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+def extract_solution(board):
+    """Extract the solution from the chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return solution
 
 
-solve(n)
+def mark_conflicts(board, row, col):
+    """Mark conflicts on the chessboard.
+    All positions where queens can no longer
+    be placed without attacking each other are marked.
+    Args:
+        board (list): The current chessboard state.
+        row (int): The row where a queen was placed.
+        col (int): The column where a queen was placed.
+    """
+    # Mark all positions to the right
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # Mark all positions to the left
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    # Mark all positions below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # Mark all positions above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # Mark all positions diagonally down to the right
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # Mark all positions diagonally up to the left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+    # Mark all positions diagonally up to the right
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # Mark all positions diagonally down to the left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+
+
+def solve_nqueens(board, row, queens, solutions):
+    """Recursively solve the N-queens problem.
+    Args:
+        board (list): The current chessboard state.
+        row (int): The current row being processed.
+        queens (int): The current count of placed queens.
+        solutions (list): A list of solutions.
+    Returns:
+        solutions
+    """
+    if queens == len(board):
+        solutions.append(extract_solution(board))
+        return solutions
+
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            temp_board = deep_copy_board(board)
+            temp_board[row][c] = "Q"
+            mark_conflicts(temp_board, row, c)
+            solutions = solve_nqueens(temp_board, row + 1,
+                                      queens + 1, solutions)
+
+    return solutions
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = create_board(int(sys.argv[1]))
+    solutions = solve_nqueens(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
